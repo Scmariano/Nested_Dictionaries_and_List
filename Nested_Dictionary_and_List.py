35b633b8e031b2793ea9b0ab6f7e2fb24d95aa40
Scmariano <104543069+Scmariano@users.noreply.github.com>
2022-06-10 14:04:59 -0700

    commit

--- Nested_Dictionary_and_List.py
+++ Nested_Dictionary_and_List.py
@@ -46,13 +46,30 @@ def iterate_dictionary(some_list):
         print(output)
 iterate_dictionary(students)
 
+
+
+
 #3. Get Values From a List of Dictionaries
 # key = first_name, last_name
 # values = []
 
+
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'},
+    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+    {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
 def iterateDictionary2(key_name, some_list):
-    for i in some_list:
-        print(i[key_name])
+    # for i in some_list:
+    #     print(i[key_name])
+
+    for i in range (len(some_list)):
+        print(some_list[i][key_name])
+
+
+
 
 
 iterateDictionary2('first_name', students)
